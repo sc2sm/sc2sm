bd4c43c57ab80bfa49c1ad574714a90c3edd0de7
ezhong0 <edwardrzhong@gmail.com>
2025-09-26 12:40:40 -0700

    clean repo

--- routes/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Routes package for Flask blueprints.
-"""
--- services/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Services package for external API integrations and business logic.
-"""
-from .github_service import GitHubService
-from .twitter_service import TwitterService
-from .ai_service import AIService
-
-__all__ = ['GitHubService', 'TwitterService', 'AIService']
